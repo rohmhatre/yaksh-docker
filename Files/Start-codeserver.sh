63587e98022f6c7cbf37d42bc7dbc0d4f9b96361
root <roh.mhatre@gmail.com>
2017-11-02 20:43:35 +0530

    Done some changes

--- Files/Start-codeserver.sh
+++ Files/Start-codeserver.sh
@@ -2,7 +2,9 @@
 git clone https://github.com/rohmhatre/online_test.git /src/online_test
 rsync -a --remove-source-files /src/online_test/* /Sites/online_test/
 usermod -u 48 nobody
+groupmod -u 48 nobody
 chown -R nobody /Sites/online_test
 chmod -R 666 /Sites/online_test
 chmod -R +X /Sites/online_test/
-/usr/bin/sudo -su nobody python -m yaksh.code_server
+/usr/bin/sudo -su nobody python3 -m yaksh.code_server
+
